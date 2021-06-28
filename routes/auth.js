7faaf4f07e6be3ae313f43dee012ec95f78bbf16
Matheus Medeiros Sarmento <matheus.m.sarmento@gmail.com>
2021-06-27 22:26:51 -0300

    Separates code in backend and frontend

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,16 @@ module.exports = (app) => {
     passport.authenticate('google', { scope: ['profile', 'email'] }));
 
   app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
+    passport.authenticate('google', { failureRedirect: '/auth/google' }),
     (req, res) => {
       res.redirect('/');
     });
 
-  app.get('/logout', (req, res) => {
+  app.get('/api/user', (req, res) => {
+    res.send(req.user);
+  });
+
+  app.get('/signout', (req, res) => {
     req.logout();
     res.redirect('/');
   });
